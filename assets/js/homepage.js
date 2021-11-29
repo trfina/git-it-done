290defedf04a11fe439ff3e1c6bd3ef46312f739
Tammy Fina <finatammy@gmail.com>
2021-11-29 18:13:03 -0500

    6.1.6 completed

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,14 @@
 
-var getUserRepos= function() {
-    var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+var getUserRepos= function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
-        console.log("inside", response);
     });
-    console.log("outside", response);    
 };
 
-getUserRepos();
+getUserRepos("trfina");
